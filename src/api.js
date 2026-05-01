0aec014a3845afb4ab410aac56aaa7d96286a2e7
K manikanth Reddy <manikanthreddykomalla@gmail.com>
2026-05-01 21:55:07 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: `${import.meta.env.VITE_API_URL}/api` || "http://localhost:8080/api"
 });
 
 if (token) {
